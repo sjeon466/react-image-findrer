2d3a38744d601036c6625ac8e2776a81db74c63f
Seongmin <sjeon466@gmail.com>
2018-11-27 07:11:46 -0800

    implement search logic

---
+++ src/components/search/Search.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+
+class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "10820559-b15e0b1efcdf358ff93d26cf5",
+    images: []
+  };
+
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+            this.state.searchText
+          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        )
+        .then(response => this.setState({ images: response.data.hits }))
+        .catch(err => console.log(err));
+    });
+  };
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
+        />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
+      </div>
+    );
+  }
+}
+
+export default Search;
