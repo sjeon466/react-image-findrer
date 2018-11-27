96135e57081d362874874ce5bf9477b8b62a5c85
Seongmin <sjeon466@gmail.com>
2018-11-27 07:42:15 -0800

    fix zero result case

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -15,15 +15,20 @@ class Search extends Component {
   };
 
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then(response => this.setState({ images: response.data.hits }))
-        .catch(err => console.log(err));
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === "") {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(response => this.setState({ images: response.data.hits }))
+          .catch(err => console.log(err));
+      }
     });
   };
 
