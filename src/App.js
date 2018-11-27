91405ec588174d3d0dabc975d6bf778b23af50e0
Seongmin <sjeon466@gmail.com>
2018-11-27 06:51:06 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import NavBar from "./components/navbar/NavBar";
+import MuiThemeProdivder from "material-ui/styles/MuiThemeProvider";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <MuiThemeProdivder>
+        <div>
+          <NavBar />
+        </div>
+      </MuiThemeProdivder>
+    );
   }
 }
 
